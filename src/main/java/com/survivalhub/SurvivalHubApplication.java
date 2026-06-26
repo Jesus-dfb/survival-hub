f62dcb95eaccfa7e2f1ae102a62888d78173831a
Jesús <jesusdfb90@gmail.com>
2026-06-26 17:21:03 +0200

    commit inicial

---
+++ src/main/java/com/survivalhub/SurvivalHubApplication.java
@@ -0,0 +1,13 @@
+package com.survivalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivalHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/survivalhub/SurvivalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.survivalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
